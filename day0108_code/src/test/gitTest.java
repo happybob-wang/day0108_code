b38ee59065cc7de66ab54b43df672240e1e53a71
wanghao <1357271016@qq.com>
2023-07-08 19:28:34 +0800

    打印换行

--- day0108_code/src/test/gitTest.java
+++ day0108_code/src/test/gitTest.java
@@ -2,15 +2,15 @@ package test;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.print("hello git");
-        System.out.print("hello git2");
-        System.out.print("hello git3");
-        System.out.print("hello git4");
-        System.out.print("hello git4-hotfix");
-        System.out.print("hello git5-master");
-        System.out.print("hello git5-hotfix2");
-        System.out.print("hello git5-hotfix3");
-        System.out.print("hello push-test-hotfix3");
-        System.out.print("hello push-test-hotfix3 in github");
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git4-hotfix");
+        System.out.println("hello git5-master");
+        System.out.println("hello git5-hotfix2");
+        System.out.println("hello git5-hotfix3");
+        System.out.println("hello push-test-hotfix3");
+        System.out.println("hello push-test-hotfix3 in github");
     }
 }
