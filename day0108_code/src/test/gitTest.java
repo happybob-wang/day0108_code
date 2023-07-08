1144d1531ccc2b66412b2cbb402ed990f433d06a
wanghao <1357271016@qq.com>
2023-07-08 18:19:05 +0800

    third commit

--- day0108_code/src/test/gitTest.java
+++ day0108_code/src/test/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.print("hello git");
         System.out.print("hello git2");
+        System.out.print("hello git3");
     }
 }
